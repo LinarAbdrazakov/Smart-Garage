4600b4957f02e7994be19c364568b19d715fb4a7
LinarAbdrazakov <linar200015@gmail.com>
2019-11-10 17:42:10 +0300

    added light control with pwm

--- STM32/main.c
+++ STM32/main.c
@@ -41,10 +41,76 @@ static void rcc_config()
     return;
 }
 
+void gpio_config()
+{
+	/*
+     * Configure output channel for light
+     */
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+	LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_0, LL_GPIO_MODE_ALTERNATE);
+	LL_GPIO_SetAFPin_0_7(GPIOA, LL_GPIO_PIN_0, LL_GPIO_AF_2);
+}
+
+void timer_config()
+{
+	/*
+     * Setup timer to output compare mode
+     */
+	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM2);
+	LL_TIM_SetPrescaler(TIM2, 479);
+	LL_TIM_SetAutoReload(TIM2, 999);
+	LL_TIM_OC_SetCompareCH1(TIM2, 0); 
+	LL_TIM_CC_EnableChannel(TIM2, LL_TIM_CHANNEL_CH1);
+	LL_TIM_OC_SetPolarity(TIM2, LL_TIM_CHANNEL_CH1, LL_TIM_OCPOLARITY_HIGH);
+	LL_TIM_OC_SetMode(TIM2, LL_TIM_CHANNEL_CH1, LL_TIM_OCMODE_PWM1);
+	LL_TIM_SetCounterMode(TIM2, LL_TIM_COUNTERMODE_UP);
+	LL_TIM_EnableIT_CC1(TIM2);
+	LL_TIM_EnableCounter(TIM2);
+
+	/*
+     * Setup NVIC
+     */
+	NVIC_EnableIRQ(TIM2_IRQn);
+	NVIC_SetPriority(TIM2_IRQn, 1);
+}
+
+void TIM2_IRQHandler(void)
+{
+    //LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_8);
+    LL_TIM_ClearFlag_CC1(TIM2);
+}
+
+__attribute__((naked)) static void delay(void)
+{
+    asm ("push {r7, lr}");    // Сохраняем регистр-ссылку LR и R7 в стек (чтобы вернуться обратно)
+    asm ("ldr r6, [pc, #8]"); // Загружаем число 0x5b8d80 в регистр R6
+    asm ("sub r6, #1");       // вычитаем 1
+    asm ("cmp r6, #0");       // Проверяем на 0
+    asm ("bne delay+0x4");    // Если не 0, то возвращаемся на строчку 3
+    asm ("pop {r7, pc}");     // Выгружаем LR и R7 в регистры PC и R7,
+                              // тем самым возвращаясь в место вызова функции
+    asm (".word 0x007c00"); //6000000
+}
 
 int main(void)
 {
 	rcc_config();
-    while (1);
+	gpio_config();
+	timer_config();
+
+    while (1)
+    {
+    	//LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_0);
+    	for (int i = 0; i < 1000; ++i)
+    	{
+    		LL_TIM_OC_SetCompareCH1(TIM2, i); 
+    		delay();
+    	}
+    	for (int i = 999; i >= 0; --i)
+    	{
+    		LL_TIM_OC_SetCompareCH1(TIM2, i); 
+    		delay();
+    	}
+    }
     return 0;
 }
